cede88ba54cf9fabf40bcaae7f3d626f3cde1ef9
Sunkanmi Olawuwo <sunkanmiolawuwo.gas@gmail.com>
2024-09-20 16:35:25 +0100

    Implement redux for basic and multiplier counter

---
+++ reduxopedia/src/store/store.js
@@ -0,0 +1,18 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { counterReducer } from "./slice/counterSlice";
+
+export const store = configureStore({
+  reducer: {
+    counterStore: counterReducer,
+  },
+});
+
+// console.log(store.getState());
+// store.dispatch({
+//   type: "counter/increment",
+// });
+// console.log(store.getState());
+// store.dispatch({
+//   type: "counter/decrement",
+// });
+// console.log(store.getState());
